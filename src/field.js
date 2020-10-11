c4799655d9573ff37e617de62d71f73d4fa947ba
HyungjunJeon <maru105kr@gmail.com>
2020-10-11 20:43:50 +0900

    JS: Fix Binding Issue

--- src/field.js
+++ src/field.js
@@ -43,7 +43,7 @@ export default class Field {
     }
   }
 
-  onClick(event) {
+  onClick = (event) => {
     const target = event.target;
     if (target.matches(".carrot")) {
       target.remove();
@@ -52,7 +52,7 @@ export default class Field {
     } else if (target.matches(".bug")) {
       this.onItemClick && this.onItemClick("bug");
     }
-  }
+  };
 }
 
 function randomNumber(min, max) {
