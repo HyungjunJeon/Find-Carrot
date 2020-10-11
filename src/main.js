f8367f29cf8964cccfe1a5e4808e84e295ff2791
HyungjunJeon <maru105kr@gmail.com>
2020-10-12 00:38:10 +0900

    JS: Last Touch

--- src/main.js
+++ src/main.js
@@ -1,29 +1,34 @@
 "use strict";
 
 import PopUp from "./popup.js";
-import Game from "./game.js";
-
-const CARROT_COUNT = 5;
-const BUG_COUNT = 5;
-const GAME_DURATION_SECOND = 5;
+import * as sound from "./sound.js";
+import { GameBuilder, Reason } from "./game.js";
 
 const gameFinishbanner = new PopUp();
 gameFinishbanner.setClickListener(() => {
   game.start();
 });
 
-const game = new Game(5, 5, 5);
+const game = new GameBuilder()
+  .withGameDuration(5)
+  .withCarrotCount(5)
+  .withBugCount(5)
+  .build();
+
 game.setGameStopListener((reason) => {
   let message;
   switch (reason) {
-    case "cancel":
+    case Reason.cancel:
       message = "Replay❓";
+      sound.playAlert();
       break;
-    case "win":
+    case Reason.win:
       message = "YOU WON 🎉";
+      sound.playWin();
       break;
-    case "lose":
+    case Reason.lose:
       message = "YOU LOST 💩";
+      sound.playBug();
       break;
     default:
       throw new Error("not valid reason");
